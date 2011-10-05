9195b83438c5a862a1764f71dd789271c1f18ab1
Patrick Menezes <patrickwds1@gmail.com>
2011-10-05 19:23:56 -0330

    Edited src/GeradorSql.java via GitHub

--- src/GeradorSql.java
+++ src/GeradorSql.java
@@ -22,4 +22,16 @@ public class GeradorSql {
 	    
 		return "INSERT INTO " + Tabela + "(" + primeiraColunas + outrasColunas + ")" + " VALUES" + "(" + primeiroCampo + outrasCampos+")";
 }
+	public static String delete(String args) {
+		String nomeTabela = "outra_tabela";
+		return "DELETE FROM " + nomeTabela;
+	}
+	public static String update(String nomeTabela, String nomeColuna,
+			String valor) {		
+		String nameT = nomeTabela;
+		String nameC = nomeColuna;
+		String valores = valor;
+		
+		return "UPDATE " + nameT + " SET " + nameC + " = " + valores + ";";
+	}
 }
